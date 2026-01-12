c2416272b933f158237cbc68a9abb7309132878f
coderiders22 <manavrai454@gmail.com>
2026-01-12 14:12:02 +0530

    resolve routing bugs

--- backend/calculator/urls.py
+++ backend/calculator/urls.py
@@ -4,26 +4,26 @@ from .auth_views import (
     LoginView,
     LogoutView,
     MeView,
-    CSRFView,
+    CSRFView
 )
 from .views import (
     CalculateView,
     HistoryView,
     clear_history,
-    delete_history_item,
+    delete_history_item
 )
 
 urlpatterns = [
     # AUTH
-    path('auth/csrf/', CSRFView.as_view()),
-    path('auth/register/', RegisterView.as_view()),
-    path('auth/login/', LoginView.as_view()),
-    path('auth/logout/', LogoutView.as_view()),
-    path('auth/me/', MeView.as_view()),
+    path("auth/csrf/", CSRFView.as_view()),
+    path("auth/register/", RegisterView.as_view()),
+    path("auth/login/", LoginView.as_view()),
+    path("auth/logout/", LogoutView.as_view()),
+    path("auth/me/", MeView.as_view()),
 
     # CALCULATOR
-    path('calculate/', CalculateView.as_view()),
-    path('history/', HistoryView.as_view()),
-    path('history/clear/', clear_history),
-    path('history/<int:pk>/', delete_history_item),
+    path("calculate/", CalculateView.as_view()),
+    path("history/", HistoryView.as_view()),
+    path("history/clear/", clear_history),
+    path("history/<int:pk>/", delete_history_item),
 ]
